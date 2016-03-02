b6de93dfe44e716a139e95068ff4e2731d959c8a
marshallmueller <mashymue@gmail.com>
2016-03-02 14:42:08 -0600

    made the S&P 500 simulation a little more flexible, especially for backtesting

--- S&P500Prediction.py
+++ S&P500Prediction.py
@@ -1,12 +1,24 @@
 import Quandl
 import random
 import config
+# get historical data of S&P
 spindex = Quandl.get("YAHOO/INDEX_GSPC", authtoken=config.key)
 
-diff = (abs(spindex.Open - spindex.Close))
+# calculate daily difference in stock value
+diff = spindex.Close - spindex.Open
+# num of stock days in a year
+year = 252
+# years to simulate
+years = 2
+# days of simulation
+days = year * years
+# actual current value
+realval = spindex.Close[spindex.Close.values.size - 1]
+
+# set up static arrays of known data
 resetdiff = []
 resetval = []
-i = 0
+i = diff.values.size - days    # set this if you want to backtest, otherwise 0
 while i < diff.values.size:
         resetdiff.append(diff[i])
         resetval.append(spindex.Close[i])
@@ -14,21 +26,27 @@ while i < diff.values.size:
 samplesize = 10000
 finalval = 0
 y = 0
+# run simulation sample size
 while y < samplesize:
     n = 0
-    iterations = 365
-    diffarr = resetdiff
-    valuearr = resetval
+    # reset arrays for simulation
+    diffarr = resetdiff[:]
+    valuearr = resetval[:]
 
-    while n < iterations:
+    # simulate defined # of days of market action
+    while n < days:
         # get random value in array of differences and add it back to the diff array
         x = int(abs(random.random() * (len(diffarr) - 1)))
         diffarr.append(diffarr[x])
         # add the difference to the value array to try to predict what it will be
         valuearr.append(valuearr[len(valuearr) - 1] + diffarr[x])
         n += 1
-    print str(valuearr[len(valuearr) - 1])
+
     finalval += valuearr[len(valuearr) - 1]
     y += 1
-    print y
-print finalval / samplesize
+
+# calculate average estimate based on simulations
+avgest = finalval / samplesize
+print "estimate value: " + str(avgest)
+# uncomment for backtesting
+print "percent error: " + str(abs(avgest-realval) * 100 / realval) + "\nBacktesting " + str(years) + " years"
